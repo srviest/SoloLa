fix bug in audio normalization.

Feature_extraction.py
@@ -82,7 +82,8 @@ def extract_feature_of_audio_clip(audio, time_segment, sr):
     for c in time_segment_sample:
         # clipping audio signal
         audio_clip = audio[int(c[0]):int(c[1])]
-        audio_clip = audio_clip/np.max(audio_clip)
+        with np.errstate(divide='ignore', invalid='ignore'):
+            audio_clip = audio_clip/np.max(audio_clip)
         # extract features
         feature_vec = feature_extractor(audio=audio_clip, features=selected_features)
         feature_vec_all = np.concatenate((feature_vec_all,feature_vec), axis = 0)            
@@ -187,7 +188,8 @@ def main(args):
         for cs in candidate_sample:
             # clipping audio signal
             audio_clip = audio[int(cs[0]):int(cs[1])]
-            audio_clip = audio_clip/np.max(audio_clip)
+            with np.errstate(divide='ignore', invalid='ignore'):
+                audio_clip = audio_clip/np.max(audio_clip)
             # extract features
             feature_vec = feature_extractor(audio=audio_clip, features=selected_features)
             feature_vec_all = np.concatenate((feature_vec_all,feature_vec), axis = 0)          