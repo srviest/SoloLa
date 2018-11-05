df8ac3b4d2ad973f864f23495cb2d37d50971327
Ykhorizon <ykhorizon.light@gmail.com>
2018-11-05 19:32:25 +0800

    fix dependency for python3

--- prerequisites.sh
+++ prerequisites.sh
@@ -13,7 +13,7 @@
 
 path=$(pwd)
 cd ${path}
-pip3 install --upgrade pip
+#pip3 install --upgrade pip
 pip3 install cython
 pip3 install nose
 pip3 install mir_eval
@@ -22,7 +22,7 @@ pip3 install networkx
 pip3 install librosa
 pip3 install --upgrade https://github.com/Theano/Theano/archive/master.zip
 pip3 install --upgrade https://github.com/Lasagne/Lasagne/archive/master.zip
-pip3 install builtins
+#pip3 install builtins
 #pip3 install theano==1.0.3
 #pip3 install lasagne==0.2.dev1
 #brew tap MTG/essentia
