37651cca8289fa5b6ac763aef369289104ab7223
Ykhorizon <ykhorizon.light@gmail.com>
2018-11-05 19:31:50 +0800

    add test case audio

--- test.sh
+++ test.sh
@@ -1,1 +1,5 @@
-python3 main.py /Users/yujuanhu/Workplace/sinica_transcription/SoloLa_cnn_py3/test_input/Bohemian_Rhapsody_Solo.mp3
+# run a simple test case to valildate SoloLa-py3 working well
+project_root=$(pwd)
+test_data="$project_root/test_input/Bohemian_Rhapsody_SOLO_Guitarraviva_normal_speed.mp3"
+echo $test_data
+python3 main.py $test_data 
