09e292f65b239c17f1b4e84a8c0e81594bc512bb
YKhorizon <ykhorizon.light@gmail.com>
2019-08-15 20:44:58 +0800

    add example & use case script

--- docker/common_use_case.sh
+++ docker/common_use_case.sh
@@ -1,5 +1,10 @@
-# env: windows git bash
+# env: windows cmd.exe
 # execution location: solola/
-# input audio location: locate at same directory with beat_tracking.py, e.g. test.mp3 or input/test.mp3
-# output: output/[filename]/FinalNotes.txt e.g. 　output/test/FinalNotes.txt 
-docker run -ti --rm -v E:\workplace\projects\solola\solola:/solola solola-py35:prod2 python3 main.py test.mp3
+# input audio location: locate at same directory with main.py, e.g. test.mp3 or input/test.mp3
+# output: output/[filename]/FinalNotes.txt e.g. output/test/FinalNotes.txt 
+
+# image: solola-py35:prod
+docker run -ti --rm -v E:\workplace\projects\solola\solola\inputs:/solola/inputs -v E:\workplace\projects\solola\solola\outputs:/solola/outputs solola-py35:prod2 python3 main.py inputs/test.mp3
+
+# image: solola-py35:dev
+docker run -ti --rm -v E:\workplace\projects\solola\solola\inputs:/solola/inputs -v E:\workplace\projects\solola\solola\outputs:/solola/outputs solola-py35:dev python3 main.py inputs/test.mp3
